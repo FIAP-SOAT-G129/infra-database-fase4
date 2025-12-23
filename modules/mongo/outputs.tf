957dae08adb6b7e7ad57dce91f90f9cf7e2a62f4
Luan Pessoa <devluan.pessoa@gmail.com>
2025-12-23 19:06:18 -0300

    fix: correct variable reference in MongoDB connection string output

--- modules/mongo/outputs.tf
+++ modules/mongo/outputs.tf
@@ -1,4 +1,4 @@
 
 output "connection_string" {
-  value = "mongodb://${vars.username}:${vars.password}@${mongodbatlas_advanced_cluster.free_cluster.connection_strings.standard_srv}/${vars.db_name}?authSource=admin"
+  value = "mongodb://${var.username}:${var.password}@${mongodbatlas_advanced_cluster.free_cluster.connection_strings.standard_srv}/${var.db_name}?authSource=admin"
 }
