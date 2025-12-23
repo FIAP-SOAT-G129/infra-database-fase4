e58ed478753687fb6b180d92291e201a85c8243e
Luan Pessoa <devluan.pessoa@gmail.com>
2025-12-23 19:02:00 -0300

    feat: update MongoDB connection string output to include username, password, and authSource

--- modules/mongo/outputs.tf
+++ modules/mongo/outputs.tf
@@ -1,3 +1,4 @@
+
 output "connection_string" {
-  value = mongodbatlas_advanced_cluster.free_cluster.connection_strings.standard_srv
+  value = "mongodb://${vars.username}:${vars.password}@${mongodbatlas_advanced_cluster.free_cluster.connection_strings.standard_srv}/${vars.db_name}?authSource=admin"
 }
