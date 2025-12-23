82f0a742a95407543537bc163af14fedc292d28b
Luan Pessoa <devluan.pessoa@gmail.com>
2025-12-23 19:48:24 -0300

    fix: update MongoDB connection string format to use SRV and replace protocol

--- modules/mongo/outputs.tf
+++ modules/mongo/outputs.tf
@@ -1,4 +1,8 @@
-
 output "connection_string" {
-  value = "mongodb://${var.username}:${var.password}@${mongodbatlas_advanced_cluster.free_cluster.connection_strings.standard_srv}/${var.db_name}?authSource=admin"
+  value = "mongodb+srv://${var.username}:${var.password}@${replace(
+    mongodbatlas_advanced_cluster.free_cluster.connection_strings.standard_srv,
+    "mongodb+srv://",
+    ""
+  )}/${var.db_name}?authSource=admin"
+  sensitive = true
 }
